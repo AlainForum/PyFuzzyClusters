b2ef2bf5740e2a04b7df0d648bd1363e6d93196b
bm424 <bm424@cam.ac.uk>
2016-06-24 17:56:05 +0100

    Include `setup.py` for very basic installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='PyFuzzyClusters',
+    version='0.1dev',
+    pymodule='clustering',
+    license='MIT',
+    long_description=open('README.md').read(),
+
+)
